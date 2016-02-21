b71ca01f056441cdaa7031f53e2df58325ebc811
Jorrie Brettin <jorriebrettin@gmail.com>
2016-02-21 17:05:55 -0500

    Some skeleton of game.js

--- js/game.js
+++ js/game.js
@@ -1,10 +1,12 @@
 // font size
-var FONT = 32;
+var FONT = 32
  
 // map dimensions
-var ROWS =  30;
-var COLS =  30;
- 
+var ROWS =  30
+var COLS =  30
+
+var timestep = 0
+var snakeArray = [] 
 
 
 var game = new Phaser.Game(COLS * FONT, ROWS * FONT, Phaser.CANVAS, null, {
@@ -12,38 +14,54 @@ var game = new Phaser.Game(COLS * FONT, ROWS * FONT, Phaser.CANVAS, null, {
 });
 
 // the structure of the map, and the function necessary to draw it
-var map;
+var map
 
 
 
 function onKeyUp(event) {
-        // draw map to overwrite previous actors positions
-        drawMap();
-        
         // act on player input
-        var acted = false;
+        var acted = false
         switch (event.keyCode) {
                 case Phaser.Keyboard.LEFT:
-                        snake.move(DIRECTION_ENUM.LEFT)
-                        break;
+                        acted = snake.move(DIRECTION_ENUM.LEFT)
+                        break
  
                 case Phaser.Keyboard.RIGHT:
-                        snake.move(DIRECTION_ENUM.RIGHT)
-                        break;
+                        acted = snake.move(DIRECTION_ENUM.RIGHT)
+                        break
  
                 case Phaser.Keyboard.UP:
-                        snake.move(DIRECTION_ENUM.UP)
-                        break;
+                        acted = snake.move(DIRECTION_ENUM.UP)
+                        break
  
                 case Phaser.Keyboard.DOWN:
-                snake.move(DIRECTION_ENUM.DOWN)
-                        break;
+                        acted = snake.move(DIRECTION_ENUM.DOWN)
+                        break
                 // case Phaser.Keyboard.ONE:
                 //             console.log("Time going to rollback")
                 //             rollBackTime(player);
         }
 
+        if acted {
+                timestep += 1
+                updateBoard()
+        }
+}
 
+//updates view of board according to current timestep
+function updateBoard() {
+        //may need some additonal 'active snake' logic so as to color board properly
+        for snake in snakeArray{
+                var positions = snake.getPositionAtTime(timestep)
+                for position in positions{
+                        //position will be a coordinate (x,y)
+                        //update map at position
+                        //doing so will give us a boolean - whether a collision happened or not
+                        //if no collision, continue
+                        //else, handle it
+                }
+        }
+}
 
 // stub
 function preload() {
@@ -51,7 +69,7 @@ function preload() {
 
 function create() {
 	// init keyboard commands
-	game.input.keyboard.addCallbacks(null, null, onKeyUp);
+	game.input.keyboard.addCallbacks(null, null, onKeyUp)
 
 }
 
