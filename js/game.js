18d3552e79b6f29fa0336b73384fd57fc43f5a2c
Jorrie Brettin <jorriebrettin@gmail.com>
2016-02-21 18:13:08 -0500

    architecture update

--- js/game.js
+++ js/game.js
@@ -6,7 +6,8 @@ var ROWS =  30
 var COLS =  30
 
 var timeStep = 0
-var snakeArray = [] 
+var activeSnake
+var pastSnakes = [] 
 
 
 var game = new Phaser.Game(COLS * FONT, ROWS * FONT, Phaser.CANVAS, null, {
@@ -43,15 +44,33 @@ function onKeyUp(event) {
         }
 
         if acted {
-                timeStep += 1
+                timeStep++
                 updateBoard()
         }
+}
+
+function nextSnake() {
+        pastSnakes.push(activeSnake)
+        var snakeProperties = map.getSnake(pastSnakes.length)
+        if snakeProperties != null {
+                createSnakeWith(snakeProperties)
+        } else {
+                gameWin()
+        }
+}
+
+function createSnakeWith(properties){
+        var snake = 
+}
+
+function gameWin(){
+        console.log("You win!")
 }
 
 //updates view of board according to current timestep
 function updateBoard() {
         //may need some additonal 'active snake' logic so as to color board properly
-        for snake in snakeArray{
+        for snake in pastSnakes{
                 var positions = snake.getPositionAtTime(timeStep)
                 for position in positions{
                         //position will be a coordinate (x,y)
@@ -70,6 +89,7 @@ function exitBoard() {
             exitBoard()
         }
     }, 1000);
+    nextSnake()
 }
 
 // stub
