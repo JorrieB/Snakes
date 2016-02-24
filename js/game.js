f111751be5e317193a10fc66b04973adab84ceb2
gueltro <gueltro@gmail.com>
2016-02-23 23:41:56 -0500

    multiple level

--- js/game.js
+++ js/game.js
@@ -13,30 +13,48 @@ var activeSnake
 var pastSnakes = [] 
 var keyboardLock = false;
 
-// var snakeData = [{snakeLength:3, startPos:[0, 3], goalPos:[6, 2], heading:DIRECTION_ENUM.RIGHT, snakeColor:'#E5FF00'},
-//                  {snakeLength:4, startPos:[5, 0], goalPos:[2, 0], heading:DIRECTION_ENUM.LEFT, snakeColor:'#2BFF95'},
-//                  {snakeLength:5, startPos:[4, 0], goalPos:[2, 6], heading:DIRECTION_ENUM.DOWN, snakeColor:'#FF2B60'}];
+levels = []
 
-// // Original Level
-// // Create Empty wallmap
-// wallData = []
-// for (var x = 0; x < ROWS; x++) {
-//     var newRow = []
-//     for (var y = 0; y < COLS; y++) {
-//         newRow.push(0)
-//     }
-//     wallData.push(newRow)
-// }
+// Sequences of levels
+
+// Hard Level
+var snakeData = [{snakeLength:3, startPos:[0, 3], goalPos:[6, 2], heading:DIRECTION_ENUM.RIGHT, snakeColor:'#E5FF00'},
+                 {snakeLength:4, startPos:[5, 0], goalPos:[2, 0], heading:DIRECTION_ENUM.LEFT, snakeColor:'#2BFF95'},
+                 {snakeLength:5, startPos:[4, 0], goalPos:[2, 6], heading:DIRECTION_ENUM.DOWN, snakeColor:'#FF2B60'}];
+
+// Create Empty wallmap
+wallData = []
+for (var x = 0; x < ROWS; x++) {
+    var newRow = []
+    for (var y = 0; y < COLS; y++) {
+        newRow.push(0)
+    }
+    wallData.push(newRow)
+}
+
+hardLevel = {snakes:snakeData, wall:wallData}
+levels.push(hardLevel)
+
+// First level
+
+var snakeData = [{snakeLength:3, startPos:[0, 4], goalPos:[6, 4], heading:DIRECTION_ENUM.RIGHT, snakeColor:'#E5FF00'},
+                  {snakeLength:4, startPos:[4, 0], goalPos:[0, 4], heading:DIRECTION_ENUM.LEFT, snakeColor:'#2BFF95'}];
+
+firstLevel = {snakes:snakeData, walls:wallData}
+
+// Second level
 
 var snakeData = [{snakeLength:3, startPos:[0, 4], goalPos:[6, 2], heading:DIRECTION_ENUM.RIGHT, snakeColor:'#E5FF00'},
-                 {snakeLength:4, startPos:[6, 4], goalPos:[0, 2], heading:DIRECTION_ENUM.LEFT, snakeColor:'#2BFF95'}];
+                  {snakeLength:4, startPos:[6, 4], goalPos:[0, 2], heading:DIRECTION_ENUM.LEFT, snakeColor:'#2BFF95'}];
 
-console.log("Hi")
 var wallData = [[0, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 0], [0, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0]]
-console.log(wallData)
 
+secondLevel = {snakes:snakeData, wall:wallData}
+levels.push(secondLevel)
 
-var map = Map(ROWS, COLS, snakeData, wallData);
+// Third level
+
+var map = Map(ROWS, COLS, hardLevel.snakes, hardLevel.wall);
 
 var Game = {
 
